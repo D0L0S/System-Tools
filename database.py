464484483ffe0f717dd8f7e09d21470f10dd7f18
D0L0S <ricklwebb@gmail.com>
2015-05-01 16:22:53 +0100

    Create database.py
    MySQL Connection Module

---
+++ database.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+## #####################
+## Database Module
+## 30/04/2015
+## Written By D0L0S
+## #####################
+
+import mysql.connector
+
+class EnvDatabase:
+
+	def __init__(self):
+		user='User'
+		host='127.0.0.1'
+		database='Database'
+		self.cnx = mysql.connector.connect(user=self.user, host=self.host, database=self.database)
+
+	def query(self, q):
+		cursor = self.cnx.cursor()
+		cursor.execute(q)
+		columns = tuple( [d[0].decode('utf8') for d in cursor.description] )
+		result = []
+		for row in cursor:
+			result.append(dict(zip(columns, row)))
+		return result
+
+	def __del__(self):
+		self.cnx.close()
+
+
+if __name__=="__main__":
+	db=EnvDatabase()
